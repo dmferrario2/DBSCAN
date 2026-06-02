be2c6ac34e3de5ced15bfddf73bdbbf185a738e7
Davide Mauro Ferrario <98091873+dmferrario2@users.noreply.github.com>
2026-06-02 09:24:11 +0200

    Update 1a_hazard_thresholds.sh
    update reference period

--- 1a_hazard_thresholds.sh
+++ 1a_hazard_thresholds.sh
@@ -11,7 +11,7 @@ if [ -f "$file_temp_read" ]; then
 		file_temp_90_mask=${file_temp_input/.nc/_90_mask.nc}
 		file_temp_95_mask=${file_temp_input/.nc/_95_mask.nc}
 		file_temp_99_mask=${file_temp_input/.nc/_99_mask.nc}
-		cdo -yearsel,1990/2020 $file_temp_input $file_temp_ref
+		cdo -yearsel,1991/2020 $file_temp_input $file_temp_ref
 		cdo -ydrunpctl,90,15 $file_temp_ref -ydrunmin,15 $file_temp_ref -ydrunmax,15 $file_temp_ref $file_temp_90
 		cdo -ydrunpctl,95,15 $file_temp_ref -ydrunmin,15 $file_temp_ref -ydrunmax,15 $file_temp_ref $file_temp_95
 		cdo -ydrunpctl,99,15 $file_temp_ref -ydrunmin,15 $file_temp_ref -ydrunmax,15 $file_temp_ref $file_temp_99
@@ -35,7 +35,7 @@ if [ -f "$file_prec_read" ]; then
 		file_prec_90_mask=${file_prec_input/.nc/_90_mask.nc}
 		file_prec_95_mask=${file_prec_input/.nc/_95_mask.nc}
 		file_prec_99_mask=${file_prec_input/.nc/_99_mask.nc}
-		cdo -yearsel,1990/2020 $file_prec_input $file_prec_ref
+		cdo -yearsel,1991/2020 $file_prec_input $file_prec_ref
 		cdo -timpctl,90 $file_prec_ref -timmin $file_prec_ref -timmax $file_prec_ref $file_prec_90
 		cdo -timpctl,95 $file_prec_ref -timmin $file_prec_ref -timmax $file_prec_ref $file_prec_95
 		cdo -timpctl,99 $file_prec_ref -timmin $file_prec_ref -timmax $file_prec_ref $file_prec_99
@@ -59,7 +59,7 @@ if [ -f "$file_wind_read" ]; then
 		file_wind_90_mask=${file_wind_input/.nc/_90_mask.nc}
 		file_wind_95_mask=${file_wind_input/.nc/_95_mask.nc}
 		file_wind_99_mask=${file_wind_input/.nc/_99_mask.nc}
-		cdo -yearsel,1990/2020 $file_wind_input $file_wind_ref
+		cdo -yearsel,1991/2020 $file_wind_input $file_wind_ref
 		cdo -timpctl,90 $file_wind_ref -timmin $file_wind_ref -timmax $file_wind_ref $file_wind_90
 		cdo -timpctl,95 $file_wind_ref -timmin $file_wind_ref -timmax $file_wind_ref $file_wind_95
 		cdo -timpctl,99 $file_wind_ref -timmin $file_wind_ref -timmax $file_wind_ref $file_wind_99
